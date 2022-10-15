d900e258a68f1b32cbc3add91b5192a86e7d5165
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2022-10-15 17:53:20 +0200

    added configureStore

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 
-import rootReducer from './reducers'
+//import rootReducer from './reducers'
+// export const store = configureStore({ reducer: rootReducer })
 
-const store = configureStore({ reducer: rootReducer })
-// The store now has redux-thunk added and the Redux DevTools Extension is turned on
+export const store = configureStore({ reducer: {} })
