7a2a822a6c9d330a9cb860f411e82472e7bb5528
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2022-10-15 18:14:57 +0200

    added createAction

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,22 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { createAction, createReducer } from '@reduxjs/toolkit'
 
 //import rootReducer from './reducers'
 // export const store = configureStore({ reducer: rootReducer })
 
-export const store = configureStore({ reducer: {} })
+const myReducer = createReducer([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  ], {
+      
+  })
+
+const addNewContact = createAction('myValue/addNewContact')
+
+console.log(addNewContact())
+
+
+export const store = configureStore({ reducer: {
+    myValue: myReducer
+} })
