376b181e51bf67cfb94bdf41f40bd53db34307d4
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2022-10-15 21:23:41 +0200

    refactored with createSlice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,38 +1,27 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { createAction, createReducer } from '@reduxjs/toolkit'
+import { createAction, createReducer, createSlice } from '@reduxjs/toolkit'
 
-//import rootReducer from './reducers'
-// export const store = configureStore({ reducer: rootReducer })
-
-export const addNewContact = createAction('myValue/addNewContact')
-export const deleteContact = createAction('myValue/deleteContact')
-
-// export const myReducer = createReducer([
-//     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//   ], {
-//     [addNewContact]:(state, action) => state+ action.payload,
-//     [deleteContact]:(state, action) => state- action.payload,
-//   })
-
-export const myReducer = createReducer(0, {
-    [addNewContact]:(state, action) => state+ action.payload,
-    [deleteContact]:(state, action) => state- action.payload,
-  })
-
-  export const add = createAction('items/add')
-  export const remove = createAction('items/remove')
-
-console.log(addNewContact())
-
-export const itemsReducer = createReducer([], { 
-  [add]:(state, action) => [...state, action.payload],
-  [remove]:(state, action) => state- action.payload,
+const myValueSlice = createSlice({
+    name: 'myValue',
+    initialState: [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      ],
+      reducers: {
+     addNewContact(state, action){
+      return [...state, action.payload];
+     },
+     deleteContact(state, action){
+        return state.filter(contact => contact !== action.payload);
+     }
+    },
 })
 
+export const { addNewContact, deleteContact } = myValueSlice.actions;
+
+console.log(myValueSlice.actions)
 
 export const store = configureStore({ reducer: {
-    myValue: myReducer,
-    items: itemsReducer
+    myValue: myValueSlice.reducer,
 } })
